0a140ba8b2973cdc5901075c1ce181b8885ce892
Eternal_plasma <eternal.plasma1634@gmail.com>
2020-10-13 23:08:48 -0500

    Added Sword, Needs tuneing

--- src/main/java/com/conzilla/ArcaneEra/init/ModItems.java
+++ src/main/java/com/conzilla/ArcaneEra/init/ModItems.java
@@ -6,9 +6,15 @@ import java.util.List;
 import com.conzilla.ArcaneEra.util.ItemBase;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems 
 {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	public static final Item ADMANTITE_INGOT = new ItemBase("admantite_ingot");
+	public static final ToolMaterial MATERIAL_ADMANTITE = EnumHelper.addToolMaterial("material_admantite",3,1570,10.0F,4.0F,22);
+	
+	public static final ItemSword ADMANTITE_SWORD = new ToolSword("admantite_sword", MATERIAL_ADMANTITE);
 }
