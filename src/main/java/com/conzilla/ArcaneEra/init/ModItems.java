e48389d15e9707c4eea65f29b1c5a4ac4d11ba1b
Eternal_plasma <eternal.plasma1634@gmail.com>
2020-11-05 12:51:36 -0600

    added mat

--- src/main/java/com/conzilla/ArcaneEra/init/ModItems.java
+++ src/main/java/com/conzilla/ArcaneEra/init/ModItems.java
@@ -25,6 +25,7 @@ public class ModItems
 	//swords
 	public static final ToolMaterial MATERIAL_ADMANTITE = EnumHelper.addToolMaterial("material_admantite",3,1570,10.0F,5.0F,22);
 	public static final ToolMaterial MATERIAL_MYTHRIL = EnumHelper.addToolMaterial("material_mythril",3,2000,10.0F,6.0F,10);
+	public static final ToolMaterial MATERIAL_ORICHALCUM = EnumHelper.addToolMaterial("material_orichalcum",1,1,1.0F,1.0F,1); //needs values
 	
 	public static final ItemSword ADMANTITE_SWORD = new FastSword("admantite_sword", MATERIAL_ADMANTITE);
 	public static final ItemSword MYTHRIL_SWORD = new SlowSword("mythril_sword", MATERIAL_MYTHRIL);
